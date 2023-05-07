6dcbf31f32b4709ef95aedd128e8275a0e13af45
Adham Omari <adhamomarics@gmail.com>
2023-05-07 20:11:31 +0300

    add-change

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -14,7 +14,7 @@ function Footer () {
   return (
     <footer className={`bg-dark footer `}>
       <a
-        href='https://api.whatsapp.com/send?phone=51955081075&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Varela%202.'
+        href='https://api.whatsapp.com/send?phone=0796087362&text=hello'
         className='float'
         target='_blank'
         rel='noreferrer'
